e14fecc250ca6e31573f778529336d712b13b06c
Tayla Phillips <richardstayla@gmail.com>
2023-11-10 16:41:39 -0600

    Complete functions exercises

--- functions.js
+++ functions.js
@@ -4,20 +4,54 @@
 // If a function takes parameters, call the function at least twice, passing different arguments each time.
 
 // 1: Write a function named greeting that returns a string with a general greeting. 
+function greeting(){
+return "What's Shaking"
+}
+var salutations = greeting();
+
+console.log(greeting());
 
 
 // 2: Write a function named customGreeting that returns a greeting WITH a specific name.
+function customGreeting(name){
+    console.log (`What's shaking, ${name}!`);
+}
+
+var greeting1 = customGreeting("Bellamy");
+var greeting2 = customGreeting("Taylor");
 
 
 // 3: Write a function named greetPerson that takes in 3 strings, a first, middle, and last name, and returns a sentence with the full name.
+function greetPerson(first, middle, last){
+    console.log (`Good to see you, ${first} ${middle} ${last}. How was your day?`);
+}
+var greeting1 = greetPerson("Saint", "Nicholas", "Kringle");
+var greeting2 = greetPerson("Tony", "Toni", "Tone");
+
 
 
 // 4: Write a function named square that takes in one number, and returns the square of that number.
 // BONUS: Print a sentence that interpolates the return value of your square function.
+function square(num){
+    var numSquared = num * num;
+    console.log(numSquared);
+    console.log(`The square of ${num} is ${numSquared}!`);
+}
 
+// I ran `square(2)' to test the function. The console printed "4" and "The square of 2 is 4!"
 
 // 5: Write a function named checkStock that satisfies the following interaction pattern:
 // Hint: You will only write one checkStock function that checks the quantity and then prints the corresponding statement.
+function checkStock(num, item){
+    var inventory = num;
+    if (inventory >= 4) {
+        console.log (`${item} is stocked`);
+    }  else if (inventory === 0) {
+        console.log (`${item} - OUT of stock!`);
+    } else if (inventory <= 3) {
+        console.log (`${item} - running LOW`);
+    }}
+
 
 checkStock(4, "Coffee");
 // => "Coffee is stocked"
